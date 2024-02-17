1eebfe923754cae628c9e3da084c88c316a837b9
Alina906745 <132589219+Alina906745@users.noreply.github.com>
2024-02-17 14:16:09 +0700

    Add files via upload

---
+++ startwindowform.h
@@ -0,0 +1,31 @@
+#ifndef STARTWINDOWFORM_H
+#define STARTWINDOWFORM_H
+
+#include <QWidget>
+
+namespace Ui {
+    class StartWindowForm;
+}
+
+class StartWindowForm : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit StartWindowForm(QWidget* parent = nullptr);
+    ~StartWindowForm();
+
+public slots:
+    void on_SignInButton_clicked();
+    void on_SignUpButton_clicked();
+
+signals:
+    void SignInRequest();
+    void SignUpRequest();
+
+private:
+    Ui::StartWindowForm* ui;
+};
+
+#endif 
+
